29218062dce5e7c9eac2f6e212a1e4e706fee74e
Kamrul Islam Nayeem <naeem8541@gmail.com>
2022-12-24 19:25:54 +0600

    Finsweet

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".clients_slick").slick({
+        slidesToShow:1,
+        slidesToScroll: 1,
+        speed: 900,
+        nextArrow: ".next",
+        prevArrow: ".prev"
+    })
+})
